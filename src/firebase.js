f60811b05bcc939c051a8c71aa0f2520029dabbe
Yatnesh <yatnesh65@gmail.com>
2026-03-19 16:35:01 +0530

    fix: add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAbkBqoaC-05vsPd8eejaw1p-fxy7ePq14",
+  authDomain: "weather-app-2a0af.firebaseapp.com",
+  projectId: "weather-app-2a0af",
+  storageBucket: "weather-app-2a0af.firebasestorage.app",
+  messagingSenderId: "118487049528",
+  appId: "1:118487049528:web:1cc50f0cb6a41819830dcd",
+  measurementId: "G-LEN9ELDLW1"
 };
 
 const app = initializeApp(firebaseConfig);
